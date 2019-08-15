af63df13492d6fccf8ae99742614bfe04a7a1787
Abagail Hudson <abagailanne@gmail.com>
2019-08-14 22:09:53 -0400

    added migrations

---
+++ db/migrate/20190815020757_create_contributions.rb
@@ -0,0 +1,4 @@
+class CreateContributions < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
---
+++ db/migrate/20190815020816_create_contributions_groups.rb
@@ -0,0 +1,4 @@
+class CreateContributionsGroups < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
